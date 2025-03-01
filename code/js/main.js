2e9d6684d24eb8b96ba6170486317025b925b0c4
Emillyn <emillyn2003@gmail.com>
2025-03-01 02:51:16 -0300

    mudança barra lateral

--- code/js/main.js
+++ code/js/main.js
@@ -1,29 +1,76 @@
-let navbarlinks = select('#menu-desktop .scrollto', true)
-const navbarlinksActive = () => {
-  let position = window.scrollY + 200
-  navbarlinks.forEach(navbarlink => {
-    if (!navbarlink.hash) return
-    let section = select(navbarlink.hash)
-    if (!section) return
-    if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
-      navbarlink.classList.add('active')
-    } else {
-      navbarlink.classList.remove('active')
-    }
-  })
-}
-window.addEventListener('load', navbarlinksActive)
-onscroll(document, navbarlinksActive)
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfólio de Emillyn</title>
+    
+    <!-- google fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <!-- CSS -->
+    <link rel="stylesheet" href="style.css">
+    <!-- CSS ICONS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
+    
+</head>
+<body>
+    <header>            
+            <div class="interface">
+                <div class="Profile">
+                    <a href="#">
+                        <img src="images/emy.jpg" alt="Profile do meu portfólio">
+                        <h1 class="text-light"><a href="index.html">Emillyn Panserini</a></h1>
+                    </a>
+                </div> <!--Profile-->
 
+                <!-- botões do linkedIn e GitHub agrupados -->
+                <div class="botoes-sociais">
+                    <div class="linkedin">
+                        <a href="https://www.linkedin.com/in/emillyn-panserini-0977a0232/" target="_blank" class="botao-foto" aria-label="LinkedIn">
+                            <img src="images/linkedin-icon-png.png" alt="Ícone do LinkedIn">
+                        </a>
+                    </div>
+                    <div class="github">
+                        <a href="https://github.com/EmillynPanserini" target="_blank" class="botao-foto" aria-label="GitHub">
+                            <img src="images/githubIcon.png" alt="Ícone do GitHub">
+                        </a>
+                    </div>
+                </div> <!--botoes-sociais-->  
 
-/* 
+               <!--<div class="close-navbar">
+                <a href="#">
+                    <button id="toggle-button">X</button> 
+                </a>
+                </div><--botao fechar navbar-->     
+                         
+                <nav class="menu-desktop">  
+                    <ul>
+                        <li><a href="#" ><i class="bi bi-house"></i>Home</a></li>
+                        <li><a href="#" ><i class="bi bi-person"></i>Sobre</a></li>
+                        <li><a href="#" ><i class="bi bi-journal"></i>Currículo</a></li>
+                        <li><a href="#" ><i class="bi bi-journal-code" style='color:#e3e3e4'  ></i>Projetos</a></li>
+                        <li><a href="#" ><i class="bi bi-hdd-stack"></i>Habilidades</a></li>
+                        <li><a href="#" ><i class="bi bi-chat-dots"></i>Contato</a></li>
+                    </ul>
+                </nav> <!--menu-desktop-->
+            </div> <!--interface -->
+    </header>
 
-Botão da menu-desktop
+    <main>
+        <section class="topo-do-site">
+            <div class="corpo">
+                <div class="flex">
+                    <div class="text-topo-site">
+                        <h1>Emillyn Panserini</h1>
+                        <p>Desenvolvedora Front-End | Estudante de Análise e Desenvolvimento de Sistemas</p>
+                    </div> <!--text-topo-site-->
 
-*/
-
-on('click', '.mobile-nav-toggle', function(e) {
-    select('body').classList.toggle('mobile-nav-active')
-    this.classList.toggle('bi-list')
-    this.classList.toggle('bi-x')
-  })
+                    
+                </div> <!--flex-->
+            </div> <!--interface-->
+        </section> <!--topo-do-site-->
+    </main>
+</body>
+</html>
