4fd63be9f6bfb073626acf48a31dd9927066681e
Emillyn <emillyn2003@gmail.com>
2025-02-28 22:29:19 -0300

    barra lateral semi

---
+++ code/js/main.js
@@ -0,0 +1,29 @@
+let navbarlinks = select('#menu-desktop .scrollto', true)
+const navbarlinksActive = () => {
+  let position = window.scrollY + 200
+  navbarlinks.forEach(navbarlink => {
+    if (!navbarlink.hash) return
+    let section = select(navbarlink.hash)
+    if (!section) return
+    if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+      navbarlink.classList.add('active')
+    } else {
+      navbarlink.classList.remove('active')
+    }
+  })
+}
+window.addEventListener('load', navbarlinksActive)
+onscroll(document, navbarlinksActive)
+
+
+/* 
+
+Botão da menu-desktop
+
+*/
+
+on('click', '.mobile-nav-toggle', function(e) {
+    select('body').classList.toggle('mobile-nav-active')
+    this.classList.toggle('bi-list')
+    this.classList.toggle('bi-x')
+  })
--- code/menuSpeed.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('toggle-button').addEventListener('click', function() {
-    var sideBar = document.querySelector('.interface');
-    sideBar.classList.toggle('open');
-});
